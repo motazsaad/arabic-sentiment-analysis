cd4ff91d33f8579402e551fd63c83fad4c8137a3
Motaz Saad <motaz.saad@gmail.com>
2018-12-07 16:35:30 +0200

    add data, code (stable version) with NLTK and SKLearn libs

---
+++ sa_sklearn.py
@@ -0,0 +1,89 @@
+from sklearn import metrics
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+import random
+from sklearn.metrics import accuracy_score
+
+
+data = []
+data_labels = []
+
+positive_file = 'arabic_tweets_txt/positive_tweets_arabic_20181207_100.txt'
+negative_file = 'arabic_tweets_txt/negative_tweets_arabic_20181207_100.txt'
+
+print('read data ...')
+# read positive data
+with open(positive_file, encoding='utf-8') as f:
+    for i in f:
+        data.append(i)
+        data_labels.append('pos')
+
+# read negative data
+with open(negative_file, encoding='utf-8') as f:
+    for i in f:
+        data.append(i)
+        data_labels.append('neg')
+
+print('data size', len(data_labels))
+print('# of positive', data_labels.count('pos'))
+print('# of negative', data_labels.count('neg'))
+
+print('text to word-frequency vectors')
+vectorizer = CountVectorizer(
+    analyzer = 'word',
+    lowercase = False,
+    ngram_range = (1, 2),
+    min_df = 3
+)
+
+print('transform data to vectors')
+features = vectorizer.fit_transform(data)
+print('represent features as array')
+features_nd = features.toarray()  # for easy usage
+test_percentage = 0.2
+print('split the data into train ({}%) and test ({}%)'.format(
+    (1-test_percentage)*100, test_percentage*100))
+X_train, X_test, y_train, y_test = train_test_split(
+        features_nd,
+        data_labels,
+        test_size=test_percentage,
+        random_state=42,
+        stratify=data_labels)
+######################################################
+
+print('define classifier')
+# classifier = LogisticRegression()
+classifier = MultinomialNB()
+# classifier = GaussianNB()
+print('classifier:', classifier.__class__)
+
+print('train ...')
+classifier = classifier.fit(X=X_train, y=y_train)
+print('make predictions on test data')
+y_pred = classifier.predict(X_test)
+
+
+print('accuracy:')
+print(accuracy_score(y_test, y_pred))
+print('classifier:', classifier.__class__)
+print('accuracy:', accuracy_score(y_test, y_pred));
+print(metrics.classification_report(y_test, y_pred))
+
+# print('display random predictions from the test data')
+# j = random.randint(0, len(X_test)-7)
+# for i in range(j, j+7):
+#     label = y_pred[0]
+#     ind = features_nd.tolist().index(X_test[i].tolist())
+#     text = data[ind].strip()
+#     print('label:', label, 'text:', text)
+
+
+print('test example')
+docs_new = ['انا احب البيتزا ❤ ❤ ', 'المعنويات عالية :) 👌 ',
+            'انا بكره الروتين ']
+X_new_counts = vectorizer.transform(docs_new)
+predicted = classifier.predict(X_new_counts.toarray())
+for doc, category in zip(docs_new, predicted):
+    print('label: {}\tdoc: {}'.format(category, doc))
---
+++ sentiment_analysis_nltk.py
@@ -0,0 +1,130 @@
+
+import collections
+import itertools
+import random
+import re
+import string
+from collections import Counter
+
+import nltk
+from nltk.metrics.scores import f_measure
+from nltk.tokenize import TweetTokenizer
+from nltk.util import ngrams
+
+
+def get_n_grams(word_list, n=2):
+    my_grams = []
+    uni_grams = [word for word in word_list]
+    for i in range(2, n + 1):
+        my_grams += ngrams(word_list, i)
+    grams = ['_'.join(g) for g in my_grams]
+    return uni_grams + grams
+
+
+def pre_process(text):
+    my_tokenizer = TweetTokenizer(reduce_len=True)
+    text = my_tokenizer.tokenize(text)
+    english_punt = list(string.punctuation)
+    text = [w for w in text if (w not in english_punt) or (len(w) >= 2)]
+    # remove double chars
+    text = [re.sub(r'([a-z])\1+', r'\1', w) for w in text]
+    # remove double punctuations
+    text = [re.sub(r'([^\P{P}-])\1+', r'\1', w) for w in text]
+    text = get_n_grams(text, n=2)
+    return text
+
+
+def document_features(document, corpus_features):
+    document_words = set(document)
+    features = {}
+    for word in corpus_features:
+        features['contains({})'.format(word)] = (word in document_words)
+    return features
+
+
+data = []
+data_labels = []
+
+positive_file = 'arabic_tweets_txt/positive_tweets_arabic_20181207_300.txt'
+negative_file = 'arabic_tweets_txt/negative_tweets_arabic_20181207_300.txt'
+
+print('read data ...')
+# read positive data
+with open(positive_file, encoding='utf-8') as f:
+    for i in f:
+        data.append(i)
+        data_labels.append('pos')
+
+# read negative data
+with open(negative_file, encoding='utf-8') as f:
+    for i in f:
+        data.append(i)
+        data_labels.append('neg')
+
+print('data size', len(data_labels))
+print('# of positive', data_labels.count('pos'))
+print('# of negative', data_labels.count('neg'))
+
+label_names = ['negative', 'positive']
+
+dataset = [(t, l) for t, l in zip(data, data_labels)]
+random.shuffle(dataset)
+# print (dataset[:10])  # see the first 10 instances
+
+
+# all_features = sum(tweets, [])
+all_features = list(itertools.chain.from_iterable(data))
+# print(set(all_features.sort())) # check sorted features
+
+# count frequencies
+all_features_freq = nltk.FreqDist(all_features)
+# print(':( freq:', all_features_freq.freq(':('))
+# print(all_features_freq.items())
+print('features frequencies are computed')
+thr = 3
+print('selecting features')
+###################################
+# remove features that have frequency below the threshold
+features_to_remove = set([k for k, v in collections.Counter(all_features_freq).items() if v < thr ])
+my_features = set([word for word in  all_features if all_features_freq.freq(word) > thr])
+my_features = set([f for f in all_features if f not in features_to_remove])
+###################################
+# my_features = list(all_features_freq)[:3000]  # top 3k features
+###################################
+print(len(my_features), 'grams are kept out of', len(all_features))
+print('features are selected')
+
+print('generating features for documents ...')
+feature_sets = [(document_features(d, my_features), c) for (d,c) in dataset]
+
+
+print('splitting documents into train and test ...')
+print('dataset size', len(data_labels))
+train_percentage = 0.8
+splitIndex = int(len(dataset) * train_percentage)
+train_set, test_set = feature_sets[:splitIndex], feature_sets[splitIndex:]
+y_train = [l for t, l in train_set]
+y_test = [l for t, l in test_set]
+
+print('dataset:', Counter(data_labels))
+print('train:', Counter(y_train))
+print('test:', Counter(y_test))
+
+print('training NaiveBayes classifier ...')
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+print('training is done')
+
+ref_sets = collections.defaultdict(set)
+test_sets = collections.defaultdict(set)
+
+for i, (feats, label) in enumerate(test_set):
+    ref_sets[label].add(i)
+    observed = classifier.classify(feats)
+    test_sets[observed].add(i)
+
+print('accuracy:', nltk.classify.accuracy(classifier, test_set))
+print('positive f-score:', f_measure(ref_sets['positive'], test_sets['positive']))
+print('negative f-score:', f_measure(ref_sets['negative'], test_sets['negative']))
+print('neutral f-score:', f_measure(ref_sets['neutral'], test_sets['neutral']))
+
+classifier.show_most_informative_features(20)
