aa105356a69cbf888b385d105f31ce3f5f63299f
Motaz Saad <motaz.saad@gmail.com>
2019-11-04 05:22:29 +0200

    add emoji overlap finder

---
+++ emoji_lex/find_overlap.py
@@ -0,0 +1,13 @@
+pos_emoji_file = 'positive_emoji_v3.txt'
+neg_emoji_file = 'negative_emoji_v3.txt'
+
+
+def load_emoj_lex(emoji_file):
+    emojis = open(emoji_file, encoding='utf-8').read().split('\n')
+    return set(emojis)
+
+
+pos_emojis = load_emoj_lex(pos_emoji_file)
+neg_emojis = load_emoj_lex(neg_emoji_file)
+
+pos_emojis.intersection(pos_emojis)
