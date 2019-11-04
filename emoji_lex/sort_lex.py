8b9aae5c15ba5314998ceb4133ac53d50ba2d9a9
Motaz Saad <motaz.saad@gmail.com>
2019-11-04 06:26:28 +0200

    add v3 lex (after sorting)

--- emoji_lex/sort_lex.py
+++ emoji_lex/sort_lex.py
@@ -4,4 +4,20 @@ neg_emoji_file = 'negative_emoji_v3.txt'
 
 def load_emoj_lex(emoji_file):
     emojis = open(emoji_file, encoding='utf-8').read().split('\n')
-    return set(emojis)
+    return set(sorted(emojis))
+
+
+pos_emojis = load_emoj_lex(pos_emoji_file)
+neg_emojis = load_emoj_lex(neg_emoji_file)
+
+
+def sort_lex(emo_set, emo_file):
+    with open(emo_file, encoding='utf-8', mode='w') as file_writer:
+        for emo in emo_set:
+            file_writer.write(emo + '\n')
+
+
+sort_lex(pos_emojis, pos_emoji_file)
+sort_lex(neg_emojis, neg_emoji_file)
+
+print('all done')
