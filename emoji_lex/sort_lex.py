e77e4621895a3fbcf81e8171d9fd26b53a22e19c
Motaz Saad <motaz.saad@gmail.com>
2019-11-05 06:36:44 +0200

    add emoji v3 unicode only lex before sorting

--- emoji_lex/sort_lex.py
+++ emoji_lex/sort_lex.py
@@ -1,6 +1,8 @@
+# pos_emoji_file = 'positive_emoji_v3.txt'
+# neg_emoji_file = 'negative_emoji_v3.txt'
+
 pos_emoji_file = 'positive_emoji_v3.txt'
 neg_emoji_file = 'negative_emoji_v3.txt'
-
 
 def load_emoj_lex(emoji_file):
     emojis = open(emoji_file, encoding='utf-8').read().split('\n')
