2334cdf07c4367fac0052f6c549da90931fd89f7
Motaz Saad <motaz.saad@gmail.com>
2019-11-19 20:20:16 +0200

    add code to filter 3 labels (notebook)

--- filter_mixed.py
+++ filter_mixed.py
@@ -32,6 +32,10 @@ def load_lexicon(lex_file):
     return emoji_lex
 
 
+def check_emo_in_lex(emos, lex):
+    return any([emo in lex for emo in emos])
+
+
 pos_file = 'arabic_tweets_tsv/20191104/Arabic_tweets_positive_20191104.tsv'
 neg_file = 'arabic_tweets_tsv/20191104/Arabic_tweets_negative_20191104.tsv'
 
@@ -42,3 +46,23 @@ print(len(pos_data), len(neg_data))
 pos_lex = load_lexicon('emoji_lex/positive_emoji_v3.txt')
 neg_lex = load_lexicon('emoji_lex/negative_emoji_v3.txt')
 print(len(pos_lex), len(neg_lex))
+
+new_pos = list()
+new_neg = list()
+mixed = list()
+
+for tweet in pos_data:
+    emojis = extract_emo_from_text(tweet)
+    if check_emo_in_lex(emojis, pos_lex) and check_emo_in_lex(emojis, neg_lex):
+        mixed.append(tweet)
+    else:
+        new_pos.append(tweet)
+
+for tweet in neg_data:
+    emojis = extract_emo_from_text(tweet)
+    if check_emo_in_lex(emojis, pos_lex) and check_emo_in_lex(emojis, neg_lex):
+        mixed.append(tweet)
+    else:
+        new_neg.append(tweet)
+
+print(len(new_pos), len(new_neg), len(mixed))
