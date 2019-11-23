aed106f5ef582cd56bf27124160f721e9253f35e
Motaz Saad <motaz.saad@gmail.com>
2019-11-23 08:28:11 +0200

    add mix label filter

--- filter_mixed.py
+++ filter_mixed.py
@@ -2,6 +2,7 @@ import emoji
 # This regex implementation is backwards-compatible with the standard ‘re’ module, but offers additional functionality.
 import regex
 from collections import Counter
+import random
 
 
 def read_tsv(data_file):
@@ -20,7 +21,6 @@ def read_tsv(data_file):
 def extract_emo_from_text(text):
     emoji_list = []
     tokens = regex.findall(r'\X', text)
-    # print(tokens)
     for word in tokens:
         if any(char in emoji.UNICODE_EMOJI for char in word):
             emoji_list.append(word)
@@ -36,33 +36,63 @@ def check_emo_in_lex(emos, lex):
     return any([emo in lex for emo in emos])
 
 
-pos_file = 'arabic_tweets_tsv/20191104/Arabic_tweets_positive_20191104.tsv'
-neg_file = 'arabic_tweets_tsv/20191104/Arabic_tweets_negative_20191104.tsv'
+def label_mix(pos_data, neg_data):
+    new_pos = list()
+    new_neg = list()
+    mixed = list()
+    all_tweets = list()
+    for tweet in pos_data:
+        emojis = extract_emo_from_text(tweet)
+        if check_emo_in_lex(emojis, pos_lex) and check_emo_in_lex(emojis, neg_lex):
+            mixed.append(tweet)
+        else:
+            new_pos.append(tweet)
+    for tweet in neg_data:
+        emojis = extract_emo_from_text(tweet)
+        if check_emo_in_lex(emojis, pos_lex) and check_emo_in_lex(emojis, neg_lex):
+            mixed.append(tweet)
+        else:
+            new_neg.append(tweet)
+    print('after:', 'pos', len(new_pos), 'neg', len(new_neg), 'mix', len(mixed))
+    for tweet in new_pos:
+        all_tweets.append('pos\t' + tweet)
+    for tweet in new_neg:
+        all_tweets.append('neg\t' + tweet)
+    for tweet in mixed:
+        all_tweets.append('mix\t' + tweet)
+    random.shuffle(all_tweets)
+    return all_tweets
 
-pos_data, pos_labels = read_tsv(pos_file)
-neg_data, neg_labels = read_tsv(neg_file)
-print(len(pos_data), len(neg_data))
+
+def write_tsv(outfile_name, tweets):
+    outfile = open(outfile_name, encoding='utf-8', mode='w')
+    for tweet in tweets:
+        outfile.write(tweet + '\n')
+
 
 pos_lex = load_lexicon('emoji_lex/positive_emoji_v3.txt')
 neg_lex = load_lexicon('emoji_lex/negative_emoji_v3.txt')
 print(len(pos_lex), len(neg_lex))
 
-new_pos = list()
-new_neg = list()
-mixed = list()
+##############################################
+pos_file = 'arabic_tweets_tsv/20191104/Arabic_tweets_positive_20191104.tsv'
+neg_file = 'arabic_tweets_tsv/20191104/Arabic_tweets_negative_20191104.tsv'
+pos_data, pos_labels = read_tsv(pos_file)
+neg_data, neg_labels = read_tsv(neg_file)
+print('before:', 'pos', len(pos_data), 'neg', len(neg_data))
 
-for tweet in pos_data:
-    emojis = extract_emo_from_text(tweet)
-    if check_emo_in_lex(emojis, pos_lex) and check_emo_in_lex(emojis, neg_lex):
-        mixed.append(tweet)
-    else:
-        new_pos.append(tweet)
+all_tweets = label_mix(pos_data, neg_data)
+outfile_name = 'arabic_tweets_tsv/20191104/3labels/Arabic_tweets_20191104.tsv'
+write_tsv(outfile_name, all_tweets)
+###########################################
 
-for tweet in neg_data:
-    emojis = extract_emo_from_text(tweet)
-    if check_emo_in_lex(emojis, pos_lex) and check_emo_in_lex(emojis, neg_lex):
-        mixed.append(tweet)
-    else:
-        new_neg.append(tweet)
 
-print(len(new_pos), len(new_neg), len(mixed))
+pos_file = 'arabic_tweets_tsv/20190413/Arabic_tweets_positive_20190413.tsv'
+neg_file = 'arabic_tweets_tsv/20190413/Arabic_tweets_negative_20190413.tsv'
+pos_data, pos_labels = read_tsv(pos_file)
+neg_data, neg_labels = read_tsv(neg_file)
+print('before:', 'pos', len(pos_data), 'neg', len(neg_data))
+
+all_tweets = label_mix(pos_data, neg_data)
+outfile_name = 'arabic_tweets_tsv/20190413/3labels/Arabic_tweets_20190413.tsv'
+write_tsv(outfile_name, all_tweets)
